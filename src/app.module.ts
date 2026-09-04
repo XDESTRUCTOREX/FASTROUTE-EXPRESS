2876f10934e12a7da8debcb7d3b4556c36f2a356
XDESTRUCTOREX <destructorex100@gmail.com>
2026-09-04 18:33:01 -0500

    se creo modulo conductores-clientes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConductoresClientesModule } from './conductores-clientes/conductores-clientes.module';
 
 @Module({
-  imports: [],
+  imports: [ConductoresClientesModule],
   controllers: [AppController],
   providers: [AppService],
 })
