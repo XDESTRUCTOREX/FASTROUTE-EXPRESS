776cbebfe61c4d0c6d7265c082239094e1a08cef
XDESTRUCTOREX <destructorex100@gmail.com>
2026-09-04 19:17:52 -0500

    feat: configura conexion TypeORM con variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PackageModule } from './package/package.module';
 import { ConductoresClientesModule } from './conductores-clientes/conductores-clientes.module';
 
 @Module({
-  imports: [PackageModule, ConductoresClientesModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST', 'localhost'),
+        port: Number(configService.get<string>('DB_PORT', '3306')),
+        username: configService.get<string>('DB_USERNAME', 'root'),
+        password: configService.get<string>('DB_PASSWORD', ''),
+        database: configService.get<string>('DB_DATABASE', 'fastroute'),
+        autoLoadEntities: true,
+        synchronize: configService.get<string>('NODE_ENV') !== 'production',
+      }),
+    }),
+    PackageModule,
+    ConductoresClientesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
